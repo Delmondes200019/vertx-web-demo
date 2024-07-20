431fdb189dcda6bf705cfb5196cb47c5504057e8
Vitor Delmondes Araujo <delmondes0800@gmail.com>
2024-07-20 17:12:22 -0300

    Deploying http server verticle. Change java version on pom.xml

--- src/main/java/com/vertx/web/demo/vertx_stock_broker/MainVerticle.java
+++ src/main/java/com/vertx/web/demo/vertx_stock_broker/MainVerticle.java
@@ -2,8 +2,26 @@ package com.vertx.web.demo.vertx_stock_broker;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.impl.logging.Logger;
+import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
+
+  private static Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.exceptionHandler(throwable -> LOG.error("Unhandled: {}", throwable.getCause()));
+    vertx.deployVerticle(new MainVerticle(), stringAsyncResult -> {
+      if(stringAsyncResult.failed()){
+        LOG.error("Failed to deploy: {}", stringAsyncResult.cause());
+        return;
+      }
+      LOG.info("Deployed ".concat(MainVerticle.class.getName()));
+    });
+  }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
