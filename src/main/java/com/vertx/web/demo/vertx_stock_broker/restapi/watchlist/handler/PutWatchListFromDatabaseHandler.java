9a0022b8b823c8a437824520a375d65c058a130c
Vitor Delmondes Araujo <delmondes0800@gmail.com>
2024-08-04 18:31:41 -0300

    Using database transaction to execute multiple sql statements

--- src/main/java/com/vertx/web/demo/vertx_stock_broker/restapi/watchlist/handler/PutWatchListFromDatabaseHandler.java
+++ src/main/java/com/vertx/web/demo/vertx_stock_broker/restapi/watchlist/handler/PutWatchListFromDatabaseHandler.java
@@ -4,14 +4,18 @@ import com.vertx.web.demo.vertx_stock_broker.restapi.help.RouteHelper;
 import com.vertx.web.demo.vertx_stock_broker.restapi.watchlist.WatchListRestApi;
 import com.vertx.web.demo.vertx_stock_broker.restapi.watchlist.model.WatchList;
 import io.netty.handler.codec.http.HttpResponseStatus;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.sqlclient.Pool;
+import io.vertx.sqlclient.SqlConnection;
+import io.vertx.sqlclient.SqlResult;
 import io.vertx.sqlclient.templates.SqlTemplate;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -42,12 +46,22 @@ public class PutWatchListFromDatabaseHandler implements Handler<RoutingContext> 
         return parameters;
       }).collect(Collectors.toList());
 
-    SqlTemplate.forUpdate(db,
+    db.withTransaction(sqlConnection ->
+      SqlTemplate.forUpdate(sqlConnection, "DELETE FROM broker.watchlist w where w.account_id = #{account_id}")
+        .execute(Collections.singletonMap("account_id", accountId))
+        .onFailure(RouteHelper.errorHandler(routingContext, "Failed to clear watchlist for accountId: ".concat(accountId)))
+        .compose(deletionDone -> addALlForAccountId(sqlConnection, routingContext, parameterBatch))
+        .onFailure(RouteHelper.errorHandler(routingContext, "Failed to update watch list for accountId: ".concat(accountId)))
+        .onSuccess(result -> routingContext.response()
+          .setStatusCode(HttpResponseStatus.NO_CONTENT.code())
+          .end())
+    );
+  }
+
+  private Future<SqlResult<Void>> addALlForAccountId(SqlConnection client, RoutingContext routingContext, List<Map<String, Object>> parameterBatch) {
+    return SqlTemplate.forUpdate(client,
         "INSERT INTO broker.watchlist VALUES(#{account_id}, #{asset}) ON CONFLICT DO NOTHING")
       .executeBatch(parameterBatch)
-      .onFailure(RouteHelper.errorHandler(routingContext, "Failed to insert into watchlist"))
-      .onSuccess(result -> routingContext.response()
-        .setStatusCode(HttpResponseStatus.NO_CONTENT.code())
-        .end());
+      .onFailure(RouteHelper.errorHandler(routingContext, "Failed to insert into watchlist"));
   }
 }
